bc4e550b59d01b0d419e44ae80976bf4a3bc0b94
traceygordon <traceyagordon@gmail.com>
2025-02-05 21:48:23 -0500

    finished!

--- server/index.js
+++ server/index.js
@@ -8,37 +8,88 @@ const client = new pg.Client(
   "postgres://postgres:2182@localhost:5432/acme_froyo_db"
 );
 
+app.use(express.json());
 // static routes here (you only need these for deployment)
-app.use(express.static(path.join(__dirname, "..client/dist")));
+// app.use(express.static(path.join(__dirname, "..client/dist")));
 
+app.post("/api/flavors", async (req, res, next) => {
+  const { flavorName } = req.body;
+  try {
+    const response = await client.query(
+      `
+        INSERT INTO flavors (name)
+        VALUES ($1)
+        RETURNING *;
+        `,
+      [flavorName]
+    );
+
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
+});
 // app routes here
 app.get("/api/flavors", async (req, res) => {
-    try {
-        const SQL = `
+  try {
+    const response = await client.query(`
         SELECT * FROM flavors
-        `;
-        const response = await client.query(SQL);
-        res.send(response.rows);
-    } catch (ex) {
-        next()
-    }
- 
+        `);
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
 });
 
-app.get("/api/flavors/:id", (req, res) => {
-  res.send("get single flavor");
+app.get("/api/flavors/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response = await client.query(
+      `
+        SELECT * FROM flavors WHERE id=$1
+        `,
+      [id]
+    );
+
+    res.send(response.rows[0]);
+  } catch (ex) {
+    next();
+  }
 });
 
-app.post("/api/flavors", (req, res) => {
-  res.send("create flavor");
+app.delete("/api/flavors/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await client.query(
+      `
+         DELETE FROM flavors WHERE id=$1;
+        `,
+      [id]
+    );
+
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
 });
 
-app.delete("/api/flavors/:id", (req, res) => {
-  res.send("delete flavor");
-});
+app.put("/api/flavors/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { newName } = req.body;
+  try {
+    const response = await client.query(
+      `
+        UPDATE flavors SET name=$1 WHERE id=$2
+        RETURNING *;
+        `,
+      [newName, id]
+    );
 
-app.put("/api/flavors/:id", (req, res) => {
-  res.send("update flavor");
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
 });
 
 // create your init function
@@ -58,7 +109,7 @@ updated_at TIMESTAMP DEFAULT now()
 INSERT INTO flavors(name) VALUES('vanilla'), ('chocolate'), ('strawberry');
 `;
   await client.query(SQL);
- 
+
   console.log("data seeded");
 
   app.listen(3000, () => console.log("listening on port 3000"));
